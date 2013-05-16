af3c6a7201121f2f522fd4e59ca4d6f9fa0413cd
Yappli <gabriel.delepine@yappli.com>
2013-05-16 16:16:11 +0300

    Create angularDynamicStylesheets.js
    Initial release

---
+++ angularDynamicStylesheets.js
@@ -0,0 +1,44 @@
+/*
+* angularDynamicStylesheets v0.1.0
+* Copyleft 2013 Yappli
+*
+* This program is free software. It comes without any warranty, to
+* the extent permitted by applicable law. You can redistribute it
+* and/or modify it under the terms of the Do What The Fuck You Want
+* To Public License, Version 2, as published by Sam Hocevar. See
+* http://sam.zoy.org/wtfpl/COPYING for more details.
+*/
+var angularDynamicStylesheets = angular.module('DynamicStylesheets', []); // Specific module. Optional
+
+// Add the service to the module
+angularDynamicStylesheets.service('dynamicStylesheets', [
+    '$rootScope', 
+    '$compile', 
+    function($rootScope, $compile) {
+
+        var scope = angular.element('head').scope();
+   
+        var addStylesheet = function(href)
+        {
+            if(scope.stylesheets_service_dynamicStylesheets === undefined)
+            {
+                angular.element('head').scope().stylesheets_service_dynamicStylesheets = [];
+                angular.element('head').append($compile("<link data-ng-repeat='stylesheet in stylesheets_service_dynamicStylesheets' data-ng-href='{{stylesheet.href}}' rel='stylesheet' />")(scope)); // Found here : http://stackoverflow.com/a/11913182/1662766
+            }
+            else
+            {
+                for(var i in scope.stylesheets_service_dynamicStylesheets)
+                {
+                    if(scope.stylesheets_service_dynamicStylesheets[i].href == href) // Unique
+                        return;
+                }
+            }
+            
+            scope.stylesheets_service_dynamicStylesheets.push({href: href});
+        };
+
+        return {
+            add: addStylesheet,
+        };
+    }
+]);
