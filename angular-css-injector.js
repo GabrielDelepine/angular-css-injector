95838feb13c92ed1798beece5269cd4207ee6fbf
Gabriel Delépine <Yappli@users.noreply.github.com>
2014-05-07 09:10:48 +0200

    Throw an exception if HEAD's scope is undefined

--- angular-css-injector.js
+++ angular-css-injector.js
@@ -1,5 +1,5 @@
 /*
-* angular-css-injector v1.0.3 beta1
+* angular-css-injector v1.0.3 beta2
 * Written by Gabriel Delépine
 * Special thanks to (github users) : @kleiinnn
 * License: MIT
@@ -26,7 +26,10 @@ angular.module('angular.css.injector', [])
         var _initScope = function()
         {
             if(scope === undefined)
-                scope = head.scope(); // We initialise head's scope in a separated function because it is not defined at the time of the initialisation of the service.
+            {
+                if((scope = head.scope()) === undefined) // We initialise head's scope in a separated function because it is not defined at the time of the initialisation of the service.
+                    throw("angular.css.injector error : Please initialize your app in the HTML tag and be sure your page has a HEAD tag.");
+            }
         };
         
         // Used to add a CSS files in the head tag of the page
